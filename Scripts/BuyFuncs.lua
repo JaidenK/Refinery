8e50aab91f19ab686a6fc2831561f0c59a29e234
Jaiden King <jaaking@yahoo.com>
2017-07-27 02:19:57 -0700

    Adding another naphtha line

--- Scripts/BuyFuncs.lua
+++ Scripts/BuyFuncs.lua
@@ -2,42 +2,52 @@ local buyFuncs = {}
 local stats = require(script.Parent.Stats)
 local machines = require(script.Parent.Machines)
 
+function standardBuy(machine, product)
+   stats.spendCash(machine[1])
+   machine[3]:Destroy()
+   machine[4].Parent = stats.TycoonModel
+   if product then
+      stats.production[product] = stats.production[product] + machine[5]
+   end
+end
+
 function buyFuncs.buyAtmosDist(Touched)
-   stats.spendCash(machines.AtmosDist[1])
-   machines.AtmosDist[3]:Destroy()
-   machines.AtmosDist[4].Parent = stats.TycoonModel
+   standardBuy(machines.AtmosDist)
 
    machines.HeavyNaptha[3].Parent = stats.TycoonModel
+   machines.LightNaptha[3].Parent = stats.TycoonModel
 end
+
 function buyFuncs.buyHeavyNaptha(Touched)
-   stats.spendCash(machines.HeavyNaptha[1])
-   machines.HeavyNaptha[3]:Destroy()
-   machines.HeavyNaptha[4].Parent = stats.TycoonModel
-   stats.production.gasoline = stats.production.gasoline + machines.HeavyNaptha[5]
+   standardBuy(machines.HeavyNaptha,"gasoline")
 
    machines.HNHydrotreater[3].Parent = stats.TycoonModel
    machines.HNCatalyticReformer[3].Parent = stats.TycoonModel
 end
 function buyFuncs.buyHNHydrotreater(Touched)
-   stats.spendCash(machines.HNHydrotreater[1])
-   machines.HNHydrotreater[3]:Destroy()
-   machines.HNHydrotreater[4].Parent = stats.TycoonModel
-   stats.production.gasoline = stats.production.gasoline + machines.HNHydrotreater[5]
+   standardBuy(machines.HNHydrotreater,"gasoline")
 end
 function buyFuncs.buyHNCatalyticReformer(Touched)
-   stats.spendCash(machines.HNCatalyticReformer[1])
-   machines.HNCatalyticReformer[3]:Destroy()
-   machines.HNCatalyticReformer[4].Parent = stats.TycoonModel
-   stats.production.gasoline = stats.production.gasoline + machines.HNCatalyticReformer[5]
+   standardBuy(machines.HNCatalyticReformer,"gasoline")
 end
+
+function buyFuncs.buyLightNaptha(Touched)
+   standardBuy(machines.LightNaptha,"gasoline")
+
+   machines.LNHydrotreater[3].Parent = stats.TycoonModel
+   machines.LNIsoPlant[3].Parent = stats.TycoonModel
+end
+function buyFuncs.buyLNHydrotreater(Touched)
+   standardBuy(machines.LNHydrotreater,"gasoline")
+end
+function buyFuncs.buyLNIsoPlant(Touched)
+   standardBuy(machines.LNIsoPlant,"gasoline")
+end
+
 function buyFuncs.buyWalls(Touched)
-   stats.spendCash(machines.Walls[1])
-   machines.Walls[3]:Destroy()
-   machines.Walls[4].Parent = stats.TycoonModel
+   standardBuy(machines.Walls)
 end
 function buyFuncs.buyFloor(Touched)
-   stats.spendCash(machines.Floor[1])
-   machines.Floor[3]:Destroy()
-   machines.Floor[4].Parent = stats.TycoonModel
+   standardBuy(machines.Floor)
 end
 return buyFuncs
--- Scripts/Machines.lua
+++ Scripts/Machines.lua
@@ -2,9 +2,15 @@
 local machines = {
 --          {cost, full name, buy brick, machine model, production increase}
    AtmosDist = {500, "Atmospheric Distillation", workspace.BuyAtmosDist, workspace.AtmosDist},
+
    HeavyNaptha = {500, "Heavy Naptha Processing", workspace.BuyHeavyNaptha, workspace.HeavyNapthaAndGasolinePool, 1},
    HNHydrotreater = {100, "Heavy Naptha Hydrotreater", workspace.BuyHNHydrotreater, workspace.HNHydrotreater, 1},
    HNCatalyticReformer = {100, "Heavy Naptha Catalytic Reformer", workspace.BuyHNCatalyticReformer, workspace.HNCatalyticReformer, 1},
+
+   LightNaptha = {500, "Light Naptha Processing", workspace.BuyLightNaptha, workspace.LightNapthaPipe, 1},
+   LNHydrotreater = {100, "Light Naptha Hydrotreater", workspace.BuyLNHydrotreater, workspace.LNHydrotreater, 1},
+   LNIsoPlant = {100, "Light Naptha Isomerization Plant", workspace.BuyLNIsoPlant, workspace.LNIsoPlant, 1},
+
    Walls = {100, "Walls", workspace.BuyWalls, workspace.Walls},
    Floor = {100, "Floor", workspace.BuyFloor, workspace.FloorConcrete}
 }
